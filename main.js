d610bed5ccc09e71c4046f8933cb9808070f258d
xubowang <xubowall@gmail.com>
2016-04-04 22:26:37 -0700

    created http server

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 /* Hello, World! program in node.js */
-console.log("Hello, World!")
+console.log("Hello, World!");
+
+var http = require("http");
+
+http.createServer( function (request, response) {
+	// Send http header
+	// Http status 200 ok
+	// Content type: text/plain
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	// Send the response body as Hello World!
+	response.end('Hello World\n');
+
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
